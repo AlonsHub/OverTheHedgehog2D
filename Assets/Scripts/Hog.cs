b505671b9148a8536a8c1eb022d6b38aac1bcd6e
Lydia Dover <doverlydia@gmail.com>
2026-09-20 00:51:11 +0000

    good death animation
    now if only I could get him to flap like a bird when he dies...

--- Assets/Scripts/Hog.cs
+++ Assets/Scripts/Hog.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Hog : MonoBehaviour
@@ -5,6 +6,10 @@ public class Hog : MonoBehaviour
     public Rigidbody2D rb;
     [SerializeField] private Animator anim;
     [SerializeField] private float walkSpeed;
+    [SerializeField] private float blastToScreenSpeed;
+    [SerializeField] private float blastUpSpeed = 2f;
+    [SerializeField] private float ttl = 5f;
+
     [SerializeField] private Collider2D col;
 
     //state?
@@ -14,7 +19,7 @@ public class Hog : MonoBehaviour
             Impact();
             col.enabled = false;
 
-            Destroy(gameObject, 2f);
+            Destroy(gameObject, ttl);
     }
 
     public void Walk()
@@ -30,7 +35,45 @@ public class Hog : MonoBehaviour
         Debug.Log("Impact");
         anim.SetTrigger("Impact");
 
-        //start fading away now or after a delay?
+        StartCoroutine(ToScreenCoroutine());
+
+
+        // Camera camera = Camera.main;
+
+        // Vector2 dir = (camera.transform.position - transform.position).normalized * blastToScreenSpeed;
+        // // StartCoroutine(ToScreenCoroutine());
+        // rb.AddForce(dir + Vector2.up * blastUpSpeed, ForceMode2D.Impulse);
+    }
+
+    // protected IEnumerator WalkCoroutine()
+    // {
+    //     while (true)
+    //     {
+    //         transform.position += Vector3.right * walkSpeed * Time.deltaTime;
+    //         yield return null;
+    //     }
+    // }
+    protected IEnumerator ToScreenCoroutine()
+    {
+        Camera camera = Camera.main;
+        rb.simulated = true;
+
+        Vector3 dir = (camera.transform.position - transform.position).normalized;
+        dir += Random.Range(-0.3f, 0.3f) * Vector3.right;
+
+        float goUpFactor = blastUpSpeed;
+
+
+        while (gameObject != null)
+        {
+            transform.localScale += Vector3.one * blastToScreenSpeed;
+            transform.position += dir * blastToScreenSpeed * Time.deltaTime;
+            transform.position += Vector3.up * goUpFactor * Time.deltaTime;
+
+            // goUpFactor -= Time.deltaTime * Physics2D.gravity.y;
+            goUpFactor += Time.deltaTime * Physics2D.gravity.y; //gravity is neg
+            yield return null;
+        }
     }
     
 
